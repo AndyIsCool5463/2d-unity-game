cacc6b4368adec2c573974dc6000ca0c4f4126f3
AndyIsCool5463 <andyl5463@gmail.com>
2019-09-14 12:27:04 -0700

    I made a box move
    I made a box move

---
+++ 2d unity game/Assets/MovingBoxScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBoxScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 1f;
+    void Start()
+    {
+        print("Running!");
+    }
+
+    // Update is called once per frame
+    void Update () 
+ {
+  transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime); 
+ }
+}
